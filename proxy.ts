b94f75f366a4ac1b979678ac6dc69619f08a142f
Rakhim_B <xfreexbeatbox17@gmail.com>
2026-01-06 00:50:52 +0300

    final

--- proxy.ts
+++ proxy.ts
@@ -1,21 +1,23 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isPublicRoute = createRouteMatcher(["/api/webhook"]);
+const isPublicRoute = createRouteMatcher([
+  "/", // home
+  "/sign-in(.*)", // auth
+  "/sign-up(.*)",
+  "/api/webhooks/stripe", // stripe webhook
+]);
 
 export default clerkMiddleware(async (auth, req) => {
   const { userId } = await auth();
 
   if (!isPublicRoute(req) && !userId) {
-    return NextResponse.redirect(new URL("/", req.url));
+    return NextResponse.redirect(new URL("/sign-in", req.url));
   }
 
   return NextResponse.next();
 });
 
 export const config = {
-  matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ["/((?!_next|.*\\..*).*)", "/api/(.*)"],
 };
