aed2969a764c15f47de5766275c54baaf9198afc
Rakhim_B <xfreexbeatbox17@gmail.com>
2026-01-06 04:17:03 +0300

    final

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -16,11 +16,20 @@ export const ourFileRouter = {
   })
     .middleware(() => handleAuth())
     .onUploadComplete(() => {}),
-    courseAttachments: f(["text", "image", "video", "audio", "pdf"]).middleware(() => handleAuth())
+  courseAttachments: f({
+    image: { maxFileSize: "8MB" },
+    pdf: { maxFileSize: "32MB" },
+    audio: { maxFileSize: "1GB" },
+    video: { maxFileSize: "2GB" },
+    text: { maxFileSize: "4MB" },
+  })
+    .middleware(handleAuth)
     .onUploadComplete(() => {}),
-    chapterVideo: f({
-    video: { maxFileSize: "512GB", maxFileCount: 1 }
-  }).middleware(() => handleAuth())
+
+  chapterVideo: f({
+    video: { maxFileSize: "512GB", maxFileCount: 1 },
+  })
+    .middleware(() => handleAuth())
     .onUploadComplete(() => {}),
 } satisfies FileRouter;
 
