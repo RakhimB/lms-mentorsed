f2e6b0c54739994f7c58dd1430425b3856bd2675
Rakhim_B <xfreexbeatbox17@gmail.com>
2026-01-06 03:48:17 +0300

    final

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,8 @@ import { isTeacher } from "@/lib/teacher";
 const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
   "/sign-up(.*)",
-  "/api/webhooks/stripe", // ✅ REQUIRED
+  "/api/webhooks/stripe",
+  "/api/uploadthing(.*)",
 ]);
 
 const isTeacherRoute = createRouteMatcher(["/teacher(.*)"]);
@@ -27,5 +28,10 @@ export default clerkMiddleware(async (auth, req) => {
 });
 
 export const config = {
-  matcher: ["/((?!_next|.*\\..*).*)", "/api/(.*)"],
+    matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 };
